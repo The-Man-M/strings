05a174d4e559688c5a25eab9f556469225b7e69c
The-Man-M <143039832+The-Man-M@users.noreply.github.com>
2023-10-14 18:20:54 -0600

    gg

--- Program.cs
+++ Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int tamano1= 0;
+            int[] _chars = {80,117,116,111,32, 101,108,32,113,117,101,32,108,111,32,108,101,97 };
+         tamano1 = _chars.Length;
+            
+            char[] puto = new char[tamano1];
+           int tamano2 = puto.Length;   
+            int[] como = new int[tamano2];
+
+            for (int i = 0;i<puto.Length;i++)
+            {
+              puto[i]=(char)_chars[i];
+            }
+            for (int i = 0; i<puto.Length;i++)
+            {
+                como[i] = (int)puto[i];
+            }
+                Console.WriteLine(puto);
+            Console.WriteLine(string.Join("-",como));
+            
+
         }
     }
 }
